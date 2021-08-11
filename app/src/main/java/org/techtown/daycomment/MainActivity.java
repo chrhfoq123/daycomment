756d19e60239e23998d2a23d8366c7031a31bde4
chrhfoq123 <chrhfoq123@naver.com>
2021-08-11 23:09:49 +0900

    21-08-11 MainActivity 수정

--- app/src/main/java/org/techtown/daycomment/MainActivity.java
+++ app/src/main/java/org/techtown/daycomment/MainActivity.java
@@ -3,12 +3,17 @@ package org.techtown.daycomment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView title, content, comment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = findViewById(R.id.title);
+        title.setText("ㅎㅇㅎㅇㅎㅇㅎㅇㅎ");
     }
 }
