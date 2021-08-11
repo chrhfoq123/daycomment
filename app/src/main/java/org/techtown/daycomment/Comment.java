6ffb49bc928bf008a88c8d271fa62563aa06991e
박태광 <ptk725739@gmail.com>
2021-08-11 23:31:52 +0900

    21-08-11 코드정리

--- app/src/main/java/org/techtown/daycomment/Comment.java
+++ app/src/main/java/org/techtown/daycomment/Comment.java
@@ -5,6 +5,16 @@ public class Comment {
     private String comment;
     private String content;
 
+    public Comment() {
+        init();
+    }
+
+    private void init() {
+        setTitle("오늘의 속담 보세요");
+        setComment("가는말이고아야오는말이곱다");
+        setContent("설명 : 말을 곱게하면 똑같이 곱게 돌아온다");
+    }
+
     public void setTitle(String title){
         this.title = title;
     }
--- app/src/main/java/org/techtown/daycomment/MainActivity.java
+++ app/src/main/java/org/techtown/daycomment/MainActivity.java
@@ -14,17 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Comment ct = new Comment();
-        ct.setTitle("오늘의 속담");
 
         title = findViewById(R.id.title);
         title.setText(ct.getTitle());
 
-
-        ct.setComment("가는말이고아야오는말이곱다");
         comment = findViewById(R.id.comment);
         comment.setText(ct.getComment());
 
-        ct.setContent("설명 : 말을 곱게하면 똑같이 곱게 돌아온다");
         content = findViewById(R.id.content);
         content.setText(ct.getContent());
     }
