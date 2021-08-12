802f1631d4f1562ee3f0005c339a1ce11d32fc58
박태광 <ptk725739@gmail.com>
2021-08-13 00:08:11 +0900

    21-08-13 억지로 프레그먼트 구현 커밋

--- app/src/main/java/org/techtown/daycomment/MainActivity.java
+++ app/src/main/java/org/techtown/daycomment/MainActivity.java
@@ -1,14 +1,32 @@
 package org.techtown.daycomment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button next_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        next_btn = findViewById(R.id.next_btn);
+        next_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Proverbfragment proverbfragment = new Proverbfragment();
+                FragmentTransaction fragmenttransaction = getSupportFragmentManager().beginTransaction();
+                fragmenttransaction.replace(R.id.frame, proverbfragment);
+                fragmenttransaction.addToBackStack(null);
+                fragmenttransaction.commit();
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/org/techtown/daycomment/Proverbfragment.java
@@ -0,0 +1,23 @@
+package org.techtown.daycomment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Proverbfragment extends Fragment {
+
+    public Proverbfragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_proverb, container, false);
+    }
+}
